1783da2b5941ce7bd6fcb472d7d6963794210780
SergioLorenteDev <slb0011@alu.medac.es>
2025-10-08 13:02:34 +0200

    frontend conectado a backend

--- web-trello/vite.config.js
+++ web-trello/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  server: { port: 3000 } // 👈 correrá en http://localhost:3000
 })
