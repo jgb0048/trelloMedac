d0e34f6a24bab4dd0c8336c995ba130b1ba7b5bd
jgb0048 <jgb0048@alu.medac.es>
2025-11-19 14:13:37 +0100

    Update TrelloApiApplication.java

--- api-trello/src/main/java/com/medac/trello/api/TrelloApiApplication.java
+++ api-trello/src/main/java/com/medac/trello/api/TrelloApiApplication.java
@@ -1,19 +1,25 @@
 package com.medac.trello.api;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+// 👇 añade estos imports
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import static com.medac.trello.api.resources.TrelloApi.BASE_API_PATH;
 
 @SpringBootApplication
+// 👇 ajusta rutas si las tuyas son distintas
+@EnableJpaRepositories(basePackages = "com.medac.trello.api.model.repository")
+@EntityScan(basePackages = "com.medac.trello.api.model.entity")
 public class TrelloApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrelloApiApplication.class);
+        SpringApplication.run(TrelloApiApplication.class, args);
     }
 
     @Bean
@@ -21,5 +27,3 @@ public class TrelloApiApplication {
         return factory -> factory.setContextPath(BASE_API_PATH);
     }
 }
-
-
